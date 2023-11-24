5aa577ef373517b41a19758bb254bd7cf5a5f6e5
Gustavo-erades <eradesvilarinho@gmail.com>
2023-11-24 19:37:50 -0300

    começando script php

--- cadastro/validarLogin.php
+++ cadastro/validarLogin.php
@@ -1,2 +1,10 @@
 <?php
-    
+    include_once("conexao.php");
+    if(isset($_POST["enviar"])){
+        $email=$_POST["email"];
+        $senha=md5($_POST["senha"]);
+        $sql="SELECT * FROM USUARIO WHERE 
+        email='$email' AND senha='$senha'";
+        $resultado=mysqli_query($conexao,$sql);
+        echo $resultado;
+    }
