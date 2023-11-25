f989755eafc4903d3e2ceea0b1d74683bf8ae86d
Gustavo-erades <eradesvilarinho@gmail.com>
2023-11-24 23:28:16 -0300

    estilizando body dda página home

--- home.php
+++ home.php
@@ -9,19 +9,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="./style/styleHome.css">
     <title>Home-ver dados</title>
 </head>
 <body>
     <div class="container">
-        <div>
+        <div id="header">
             <div>
-                nome
+                <img src="./imagens/estacio-logo.png" alt="logo da estácio" width="40px">
+                <h1>
+                    Olá, Gustavo Êrades Vilarinho Silva
+                </h1>
             </div>
             <div>
-                idade
+                <h3>
+                    Idade: 20 anos
+                </h3>
+                <h3>
+                    Email: eradesvilarinho@gmail.com
+                </h3>
             </div>
         </div>
-        
+        <div id="div_tabela">
+            <div>
+                <table>
+                    
+                </table>
+            </div>
+        </div>
+        <div id="footer">
+            <p>
+                &copy; desenvolvido por Gustavo Êrades
+            </p>
+        </div>
     </div>
 </body>
 </html>
