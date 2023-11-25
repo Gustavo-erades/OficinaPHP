11a06370b2ed2bc23d1f2e6a09aed270cdd19ac5
Gustavo-erades <eradesvilarinho@gmail.com>
2023-11-24 22:56:55 -0300

    estilizando a home

--- home.php
+++ home.php
@@ -9,50 +9,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style/style.css">
-    <title>Ver dados</title>
+    <title>Home-ver dados</title>
 </head>
 <body>
     <div class="container">
-        <table>
-            <tr>
-                <th>
-                    Id
-                </th>
-                <th>
-                    Nome
-                </th>
-                <th>
-                    E-mail
-                </th>
-                <th>
-                    Data de nascimento
-                </th>
-                <th>
-                    Senha
-                </th>
-            </tr>
-            <?php
-                while($row=mysqli_fetch_assoc($resultado)){
-                    echo "<tr>";
-                    echo "<td>";
-                    echo $row["id"];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $row["nome"];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $row["email"];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $row["dt_nascimento"];
-                    echo "</td>";
-                    echo "<td>";
-                    echo $row["senha"];
-                    echo "</td>";
-                    echo "</tr>";
-                }
-            ?>
-        </table>
+        <div>
+            <div>
+                nome
+            </div>
+            <div>
+                idade
+            </div>
+        </div>
+        
     </div>
 </body>
 </html>
