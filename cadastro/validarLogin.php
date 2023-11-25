7f54638ae1a3a37843693570a2b62f92cbb8a40a
Gustavo-erades <eradesvilarinho@gmail.com>
2023-11-24 22:51:15 -0300

    lógica de validação de login

--- cadastro/validarLogin.php
+++ cadastro/validarLogin.php
@@ -1,10 +1,14 @@
 <?php
-    include_once("conexao.php");
+    include_once("../conexao/conexao.php");
     if(isset($_POST["enviar"])){
         $email=$_POST["email"];
         $senha=md5($_POST["senha"]);
         $sql="SELECT * FROM USUARIO WHERE 
         email='$email' AND senha='$senha'";
         $resultado=mysqli_query($conexao,$sql);
-        echo $resultado;
+        if(mysqli_num_rows($resultado)>0){
+            header("Location:../home.php");
+        }else{
+            header("Location:../login.php");
+        }
     }
